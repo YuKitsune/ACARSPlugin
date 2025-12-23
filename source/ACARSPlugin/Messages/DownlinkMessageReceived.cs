3e7e355dbcf9637906d1afbc86619da9647961db
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-12-23 13:09:42 +1000

    fix: Promote to CDA on downlink received

--- source/ACARSPlugin/Messages/DownlinkMessageReceived.cs
+++ source/ACARSPlugin/Messages/DownlinkMessageReceived.cs
@@ -1,12 +1,13 @@
 ﻿using ACARSPlugin.Model;
 using ACARSPlugin.Server.Contracts;
+using ACARSPlugin.Services;
 using MediatR;
 
 namespace ACARSPlugin.Messages;
 
 public record DownlinkMessageReceivedNotification(IDownlinkMessage DownlinkMessage) : INotification;
 
-public class DownlinkMessageReceivedNotificationHandler(MessageRepository messageRepository, IPublisher publisher)
+public class DownlinkMessageReceivedNotificationHandler(MessageRepository messageRepository, AircraftConnectionTracker aircraftConnectionTracker, IPublisher publisher)
     : INotificationHandler<DownlinkMessageReceivedNotification>
 {
     public async Task Handle(DownlinkMessageReceivedNotification notification, CancellationToken cancellationToken)
@@ -15,6 +16,17 @@ public class DownlinkMessageReceivedNotificationHandler(MessageRepository messag
             return;
 
         await messageRepository.AddDownlinkMessage(cpdlcDownlink, cancellationToken);
+
+        // Promote the aircraft to CDA
+        // The server already does this, but I'm too lazy to implement the signalr stuff
+        var connection = await aircraftConnectionTracker.GetConnectedAircraft(
+            notification.DownlinkMessage.Sender,
+            cancellationToken);
+        if (connection is not null && connection.DataAuthorityState == DataAuthorityState.NextDataAuthority)
+        {
+            connection.DataAuthorityState = DataAuthorityState.CurrentDataAuthority;
+        }
+        
         await publisher.Publish(new CurrentMessagesChanged(), cancellationToken);
     }
 }
