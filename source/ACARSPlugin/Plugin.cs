932f010e6afc105839f172698bb3842ed25f2300
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-12-30 17:11:25 +1000

    chore: Todos

--- source/ACARSPlugin/Plugin.cs
+++ source/ACARSPlugin/Plugin.cs
@@ -316,6 +316,8 @@ public class Plugin : ILabelPlugin, IRecipient<CurrentMessagesChanged>, IRecipie
         // TODO: If nobody has jurisdiction, and we're the next owner, show the message
         // TODO: If nobody has jurisdiction, and we were the last owner, show the message
 
+        // TODO: VATSIM-ISM: If the controlling sector isn't connected to the ATSU server, then show the message
+
         // Fallback: If nobody has jurisdiction, show the message to everyone
         if (!fdr.IsTracked)
         {
